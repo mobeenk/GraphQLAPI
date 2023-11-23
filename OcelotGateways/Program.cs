0b033d9c57a2376fe0afa309b77e8ff3a8e4104d
Moubien Kayali <Moubien.Kayali@cloudsolutions.com.sa>
2023-11-23 13:45:26 +0300

    fix ocelot.json add proper program.cs configuation

--- OcelotGateways/Program.cs
+++ OcelotGateways/Program.cs
@@ -3,11 +3,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 // Configure services
-builder.Services.AddOcelot(builder.Configuration);
 
+builder.Services.AddOcelot(builder.Configuration);
 // Configure the application
 var app = builder.Build();
 
